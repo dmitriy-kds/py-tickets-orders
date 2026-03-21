bed2df0e94a793bb3fc24eebcb957676a1f31706
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-21 22:11:22 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -109,9 +109,9 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
 
 
 class OrderPagination(PageNumberPagination):
-   page_size = 10
-   page_size_query_param = "page_size"
-   max_page_size = 100
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
 
 
 class OrderViewSet(viewsets.ModelViewSet):
